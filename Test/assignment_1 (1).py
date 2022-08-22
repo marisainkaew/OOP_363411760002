396b706b0b89119b9d64247dc2cf86a2e5f15dfc
marisa.i <marisa.i@rmutsvmail.com>
2022-08-22 10:57:58 +0700

    add:assignment 1

---
+++ Test/assignment_1 (1).py
@@ -0,0 +1,46 @@
+"""
+Name: {Marisa Imkaew}
+SID: {363411760002}
+Group: {MIT431}
+"""
+
+# ประกาศตัวแปร x มีค่าเท่ากับ 100
+x =100
+# แสดงผลค่าตัวแปร x
+print(type(x))
+# แสดงชนิดข้อมูลของตัวแปร x 
+x = 100
+x  # print(x)
+mylist = 100
+print(mylist)
+# ประกาศตัวแปร  y มีค่าเท่ากับ 200
+y =200
+# แสดงผลค่าตัวแปร y
+print(type(y))
+# แสดงชนิดข้อมูลของตัวแปร y 
+y = 200
+mylist =200
+print(mylist)
+# แสดงผลค่าตัวแปร x และ y โดยมีข้อความดังนี้ "x is 100 and y is 200"
+print(f"x is {x} and y is {y}")
+print(mylist)
+# หาผลรวมของตัวแปร x และ y และเก็บไว้ในตัวแปร z
+z =(x+y)
+# แสดงผลค่าตัวแปร z โดยการใช้ formatted print  -- > print(f{...})
+print(f'z= {z}')
+# หาผลลบของตัวแปร x และ y และเก็บไว้ในตัวแปร z
+z =(x-y)
+# แสดงผลค่าตัวแปร z formatted print  -- > print(f{...})
+print(f'z= {z}')
+# หาผลคูณของตัวแปร x และ y และเก็บไว้ในตัวแปร z
+z =(x*y)
+# แสดงผลค่าตัวแปร z formatted print  -- > print(f{...})
+print(f'z= {z}')
+# หาผลหารของตัวแปร x และ y และเก็บไว้ในตัวแปร z
+z =(x/y)
+# แสดงผลค่าตัวแปร z formatted print  -- > print(f{...})
+print(f'z= {z}')
+# หาผลหารแบบจำนวนเต็ม (floor division) ของตัวแปร x และ y และเก็บไว้ในตัวแปร z
+z = x//y
+# แสดงผลค่าตัวแปร z formatted print  -- > print(f{...})
+print(x/y)
